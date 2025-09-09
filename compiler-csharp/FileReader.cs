db8ed62833c649fef29c393fbc54921ea290d861
Timur Salakhov <claymix@yandex.ru>
2025-09-09 17:10:46 +0300

    feat(lexer): Fix `FileReader` to read by char

--- compiler-csharp/FileReader.cs
+++ compiler-csharp/FileReader.cs
@@ -6,7 +6,7 @@ namespace Lexer.IO
     public class FileReader
     {
         private string _filename;
-        private List<string> _content = new List<string>();
+        private StreamReader _fileStream;
 
         public FileReader(string filepath)
         {
@@ -21,28 +21,19 @@ namespace Lexer.IO
             this._filename = Path.GetFileName(filepath);
 
             // Read file content and process it
-            string[] contentLines = File.ReadAllText(filepath).Split("\n"); // Split into lines
-            foreach (string line in contentLines)
-            {
-                int indexOfComment = line.IndexOf("//"); 
-                string processedLine = indexOfComment >= 0 ? line.Substring(0, indexOfComment) : line; // Remove comments if any
-
-                string[] words = processedLine.Split(new char[] { ' ', '\t', '\r' }, StringSplitOptions.RemoveEmptyEntries); // Split into words
-                foreach (string word in words)
-                {
-                    this._content.Add(word);
-                }
-            }
+            this._fileStream = new StreamReader(filepath);
         }
-        
+
         public string GetFilename()
         {
             return this._filename;
         }
 
-        public List<string> GetContent()
+        public char? GetNextChar()
         {
-            return this._content;
+            int nextChar = this._fileStream.Read();
+            if (nextChar == -1) return null; // End of file
+            return (char)nextChar;
         }
 
     }
