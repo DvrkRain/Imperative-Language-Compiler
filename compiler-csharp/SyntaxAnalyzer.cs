5caaa22adb3e78757a37b02e661724aa0da59d6e
Timur Salakhov <claymix@yandex.ru>
2025-10-01 18:23:34 +0300

    feat(syntax): Refactor structure

--- compiler-csharp/SyntaxAnalyzer.cs
+++ compiler-csharp/SyntaxAnalyzer.cs
@@ -1,34 +1,101 @@
 using LexicalAnalyzer.TokenTree;
 
-// Namespace name manageable
 namespace SyntaxAnalyzer
 {
-    // Syntax Analyzer class name manageable
-    public class TreeSyntaxAnalyzer
+    public abstract class Node
     {
-        public Queue<Token> tokenStream;
-        public List<Node> childs;
-        public TreeSyntaxAnalyzer(Queue<Token> tokenStream)
-        {
-            this.tokenStream = tokenStream;
-            this.childs = new List<Node>();
-        }
+        protected List<Node> childs;
 
-        public void ParseStream()
-        {
-            // Return type manageable            
-        }
+        public Node() => this.childs = new List<Node>();
 
+        public abstract void Parse(ref Queue<Token> tokenQueue);
     }
 
-    public class Node
+    public class ProgramNode : Node
     {
-        public Token token;
-        public List<Node> childs;
-        public Node(Token token)
-        {
-            this.token = token;
-            this.childs = new List<Node>();
-        }
+        public bool main;
+
+        public ProgramNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public class IfNode : Node
+    {
+        public List<BranchNode> branches;
+
+        public IfNode() : base() => this.branches = new List<BranchNode>();
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public abstract class IdentifierNode : Node
+    {
+        protected string identifier;
+
+        public IdentifierNode() : base() => this.identifier = "";
+    }
+
+    public class VarNode : IdentifierNode
+    {
+        public VarNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {} 
+    }
+
+    public class TypeNode : IdentifierNode
+    {
+        public TypeNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public class RecordNode : IdentifierNode
+    {
+        public RecordNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public class ArrayNode : IdentifierNode
+    {
+        public ArrayNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public class AssignmentNode : IdentifierNode
+    {
+        public AssignmentNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public abstract class SubprogramNode : Node
+    {
+        protected ProgramNode nested;
+
+        public SubprogramNode() : base() {}
+    }
+
+    public class ForNode : SubprogramNode
+    {
+        public ForNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public class WhileNode : SubprogramNode
+    {
+        public WhileNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
+    }
+
+    public class BranchNode : SubprogramNode
+    {
+        public BranchNode() : base() { }
+
+        public override void Parse(ref Queue<Token> tokenQueue) {}
     }
 }
