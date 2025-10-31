51a8d6a9ed8a4cd7d5031eb0dd828d546edba431
Timur Salakhov <claymix@yandex.ru>
2025-10-31 21:29:29 +0300

    feat(semantic): add `Scope`

--- compiler-csharp/SymbolTable.cs
+++ compiler-csharp/SymbolTable.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-
 namespace SemanticAnalyzer.SymbolTable;
 
 public abstract class Entry
@@ -20,13 +18,11 @@ public class Variable : Entry
 {
     public string Type { get; } // Each variable has type
     public object? Value { get; } // Each variable might have value
-    public bool IsInitialized { get; } // Case `var a : integer`
     
     public Variable(string name, string type, object? value = null) : base(name)
     {
         Type = type;
         Value = value;
-        IsInitialized = value != null;
     }
 
     public override bool AddToScope() {
@@ -38,15 +34,13 @@ public class Routine : Entry
 {
     public List<Variable> Parameters { get; } // Routine parameters contained in list to keep order
     public string? ReturnType { get; } // Each routine might have a return type
-    public Hashtable? BodyScope { get; } // Each routine migh have a body scope
-    public bool IsForward { get; set; } // Determine whether body is presented
+    public Scope? BodyScope { get; } // Each routine migh have a body scope
     
     public Routine(string name, List<Variable>? parameters = null, string? returnType = null) : base(name)
     {
         Parameters = parameters ?? new List<Variable>();
         ReturnType = returnType;
         BodyScope = null;
-        IsForward = false;
     }
 
     public override bool AddToScope() {
@@ -56,8 +50,8 @@ public class Routine : Entry
 
 public class Type : Entry
 {
-    public string BaseType { get; set; } // Each type has a base type (integer, real, boolean, array, record), type == baseType -> baseType in builtInTypes
-    public Hashtable? RecordScope { get; } // Each type might have its scope
+    public string BaseType { get; } // Each type has a base type (integer, real, boolean, array, record), type == baseType -> baseType in builtInTypes
+    public Scope? RecordScope { get; } // Each type might have its scope
     
     public Type(string name, string baseType) : base(name)
     {
@@ -69,3 +63,36 @@ public class Type : Entry
     }
 }
 
+
+public class Scope
+{
+    private Dictionary<string, Entry> entries;
+    
+    public Scope? Parent { get; }
+    
+    public Scope(ref Scope? parent)
+    {
+        entries = new Dictionary<string, Entry>();
+        Parent = parent;
+    }
+
+    public bool AddEntry(Entry entry)
+    {
+        if (entries.ContainsKey(entry.Name))
+        {
+            return false;
+        }
+        entries[entry.Name] = entry;
+        return true;
+    }
+    
+    public Entry? LookupEntry(string name)
+    {
+        if (entries.TryGetValue(name, out var entry))
+        {
+            return entry;
+        }
+        
+        return Parent?.LookupEntry(name);
+    }
+}
