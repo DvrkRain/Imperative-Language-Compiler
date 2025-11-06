dd76ba7c911cdb3b4dd1208b3b622cee7e29bdaf
Uberch <vam.molch@gmail.com>
2025-11-06 16:38:16 +0300

    a

--- compiler-csharp/AST/FieldAcessNode.cs
+++ compiler-csharp/AST/FieldAcessNode.cs
@@ -1,12 +1,33 @@
 using Data.Objects;
 namespace AST {
 public class FieldAccessNode : Node {
-	public FieldAccessNode(Position pos) : base(pos) { }
+	protected bool call;
+
+	public FieldAccessNode(Position pos) : base(pos) => this.call = false;
 	public FieldAccessNode(Position pos, Node init) : this(pos) =>
 		this.childs.Add(init);
 
 	public override void Parse(ref Queue<Token> tokenQueue) {
 		Token token = tokenQueue.Peek();
+		if(token.Code() == TokenCode.left_parenthesis) {
+			this.call = true;
+			tokenQueue.Dequeue();
+			ExpressionNode expr = new ExpressionNode(tokenQueue.Peek().Position());
+			expr.Parse(ref tokenQueue);
+			this.childs.Add(expr);
+			token = tokenQueue.Peek();
+			while(token.Code() == TokenCode.comma) {
+				tokenQueue.Dequeue();
+				expr = new ExpressionNode(token.Position());
+				expr.Parse(ref tokenQueue);
+				this.childs.Add(expr);
+				token = tokenQueue.Peek();
+			}
+
+			if(token.Code() != TokenCode.right_parenthesis)
+				HandleUnexpectedToken(ref tokenQueue, token.Position());
+			return;
+		}
 		while(token.Code() == TokenCode.dot) {
 			tokenQueue.Dequeue();
 			token = tokenQueue.Peek();
