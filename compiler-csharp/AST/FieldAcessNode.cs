d3841133e9dfb73c1cdd717c7beb67ace28f185b
Uberch <vam.molch@gmail.com>
2025-11-06 15:09:55 +0300

    Fix: fix field access loop-stuck

--- compiler-csharp/AST/FieldAcessNode.cs
+++ compiler-csharp/AST/FieldAcessNode.cs
@@ -6,19 +6,17 @@ public class FieldAccessNode : Node {
 		this.childs.Add(init);
 
 	public override void Parse(ref Queue<Token> tokenQueue) {
-		while(true) {
-			Token token = tokenQueue.Peek();
-			while(token.Code() == TokenCode.dot) {
-				tokenQueue.Dequeue();
-				token = tokenQueue.Peek();
-				if(token.Code() != TokenCode.identifier) {
-					HandleUnexpectedToken(ref tokenQueue, token.Position());
-					return;
-				}
-				tokenQueue.Dequeue();
-				this.childs.Add(new PrimaryNode(token.Position(), token.Value()));
-				token = tokenQueue.Peek();
+		Token token = tokenQueue.Peek();
+		while(token.Code() == TokenCode.dot) {
+			tokenQueue.Dequeue();
+			token = tokenQueue.Peek();
+			if(token.Code() != TokenCode.identifier) {
+				HandleUnexpectedToken(ref tokenQueue, token.Position());
+				return;
 			}
+			tokenQueue.Dequeue();
+			this.childs.Add(new PrimaryNode(token.Position(), token.Value()));
+			token = tokenQueue.Peek();
 		}
 	}
 
