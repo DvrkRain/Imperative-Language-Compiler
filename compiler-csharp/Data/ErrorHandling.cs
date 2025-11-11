8fb9a8e47e955457a1d8d7e0d6d5bbb67168717b
Timur Salakhov <claymix@yandex.ru>
2025-11-11 18:12:55 +0300

    feat(semantic): integrated `verify` function into While node; modified error messages

--- compiler-csharp/Data/ErrorHandling.cs
+++ compiler-csharp/Data/ErrorHandling.cs
@@ -4,7 +4,11 @@ namespace Data.ErrorHandling;
 
 public static class ErrorHandling {
     private static List<string> _errorList = new();
-    
+
+    public static void Add(string message) {
+        _errorList.Add(message);
+    }
+
     public static int Count() {
         return _errorList.Count;
     }
@@ -16,27 +20,27 @@ public static class ErrorHandling {
     }
 
     public static void UnexpectedEOF(Position pos, string invoker) {
-        _errorList.Add($"{invoker}: End of file reached unexpectedly in command at position {pos.Row()},{pos.Col()}");
+        _errorList.Add($"{invoker}({pos.Row()},{pos.Col()}): End of file reached unexpectedly in command");
     }
 
     public static void ReturnOutsideFunction(Position pos, string invoker) {
-        _errorList.Add($"{invoker}: Return used outside the funtcion body at position {pos.Row()},{pos.Col()}.");
+        _errorList.Add($"{invoker}({pos.Row()},{pos.Col()}): Return used outside the funtcion body");
     }
 
     public static void ContinueOutsideCycle(Position pos, string invoker) {
-        _errorList.Add($"{invoker}: Continue used outside the loop body at position {pos.Row()},{pos.Col()}.");
+        _errorList.Add($"{invoker}({pos.Row()},{pos.Col()}): Continue used outside the loop body");
     }
 
     public static void BreakOutsideCycle(Position pos, string invoker) {
-        _errorList.Add($"{invoker}: Break used outside the loop body at position {pos.Row()},{pos.Col()}.");
+        _errorList.Add($"{invoker}({pos.Row()},{pos.Col()}): Break used outside the loop body");
     }
 
     public static void UnexpectedTokenException(Position pos, string invoker) {
-        _errorList.Add($"{invoker}: Unexpected token at {pos.Row()},{pos.Col()}.");
+        _errorList.Add($"{invoker}({pos.Row()},{pos.Col()}): Unexpected token");
     }
 
     public static void MismatchedParenthesis(Position pos, string invoker) {
-        _errorList.Add($"{invoker}: Mismatched parenthesis at {pos.Row()},{pos.Col()}.");
+        _errorList.Add($"{invoker}({pos.Row()},{pos.Col()}): Mismatched parenthesis");
     }
 
 }
