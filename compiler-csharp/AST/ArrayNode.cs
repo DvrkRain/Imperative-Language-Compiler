c5626efe9e6c114b29f825f8cbb31ec6795adfcf
Uberch <vam.molch@gmail.com>
2025-11-11 18:27:05 +0300

    Implement array node verifying

--- compiler-csharp/AST/ArrayNode.cs
+++ compiler-csharp/AST/ArrayNode.cs
@@ -1,7 +1,14 @@
 using Data.Objects;
+using SemanticAnalyzer.SymbolTable;
 namespace AST;
 public class ArrayNode : Node {
 	public ArrayNode(Position pos) : base(pos) {}
+
+	public override void PrintInfo(string indent) {
+		if (this.GetType().Name == "ArrayNode")
+			Console.WriteLine($"ArrayNode(childs={this.childs.Count}, pos=({this.position.Row()}, {this.position.Col()}))");
+		base.PrintInfo(indent);
+	}
 
 
 	public override void Parse(ref Queue<Token> tokenQueue) {
@@ -37,9 +44,23 @@ public class ArrayNode : Node {
 		}
 	}
 
-	public override void PrintInfo(string indent) {
-		if (this.GetType().Name == "ArrayNode")
-			Console.WriteLine($"ArrayNode(childs={this.childs.Count}, pos=({this.position.Row()}, {this.position.Col()}))");
-		base.PrintInfo(indent);
+	
+	public override void Verify(ref SymbolTable symTab) {
+		base.Verify(ref symTab);
+
+		int current_child_index = 0;
+		if(this.childs[current_child_index] is ExpressionNode expr) {
+			if(expr.Type() != "integer") {
+				ErrorHandling.Add($"ArrayNode({this.position.Row()},{this.position.Col()}): Expression representing array size should be of type integer.");
+			}
+			current_child_index = 1;
+		}
+
+		if(this.childs[current_child_index] is PrimaryNode identifier) {
+			if(symTab.FindEntry((string)identifier.value) is not SemanticAnalyzer.SymbolTable.Type) {
+				ErrorHandling.Add($"ArrayNode({this.position.Row()},{this.position.Col()}): Expected type identifier.");
+			}
+		} else
+			ErrorHandling.Add($"ArrayNode({this.position.Row()},{this.position.Col()}): Expected the PrimaryNode, got {this.childs[current_child_index].GetType().Name}.");
 	}
 }
