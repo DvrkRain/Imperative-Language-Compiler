7228bcca10ef43d9452b3d7c3c513f93bdfeced2
Timur Salakhov <claymix@yandex.ru>
2025-11-11 13:26:31 +0300

    feat(general): Add `ErrorHandling` class

---
+++ compiler-csharp/Data/ErrorHandling.cs
@@ -0,0 +1,23 @@
+﻿namespace Data.ErrorHandling;
+
+public static class ErrorHandling {
+    private static List<string> _errorList = new();
+
+    public static void Add(string message) {
+        _errorList.Add(message);
+    }
+    
+    public static int Count() {
+        return _errorList.Count;
+    }
+
+    public static void Clear() {
+        _errorList.Clear();
+    }
+    
+    public static void PrintErrors() {
+        foreach (string error in _errorList) {
+            Console.WriteLine(error);
+        }
+    }
+}
