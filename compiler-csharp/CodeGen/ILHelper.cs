c193fe68614a6dbe72c0b7efab86076e1f7b1a2f
Uberch <vam.molch@gmail.com>
2025-11-22 18:34:40 +0300

    Fix(codegen): Fix type conversion value cast

--- compiler-csharp/CodeGen/ILHelper.cs
+++ compiler-csharp/CodeGen/ILHelper.cs
@@ -5,16 +5,31 @@ namespace CodeGen
 {
     public static class ILHelper
     {
-        // Load integer constant (optimized)
         public static void EmitLoadInt(ILGenerator il, int value) =>
 			il.Emit(OpCodes.Ldc_I4, value);
         
-        // Load boolean constant
+        public static void EmitLoadInt(ILGenerator il, float value) =>
+			il.Emit(OpCodes.Ldc_I4, (int)Math.Round(value, MidpointRounding.AwayFromZero));
+        
+        public static void EmitLoadInt(ILGenerator il, bool value) =>
+			il.Emit(OpCodes.Ldc_I4, value?1:0);
+        
+        public static void EmitLoadReal(ILGenerator il, int value) =>
+            il.Emit(OpCodes.Ldc_R8, (float)value);
+        
+        public static void EmitLoadReal(ILGenerator il, float value) =>
+            il.Emit(OpCodes.Ldc_R8, value);
+        
+        public static void EmitLoadReal(ILGenerator il, bool value) =>
+            il.Emit(OpCodes.Ldc_R8, value?1f:0f);
+        
+        public static void EmitLoadBool(ILGenerator il, int value) =>
+            il.Emit(value != 0 ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+        
+        public static void EmitLoadBool(ILGenerator il, float value) {
+			return; }
+        
         public static void EmitLoadBool(ILGenerator il, bool value) =>
             il.Emit(value ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
-        
-        // Load real constant
-        public static void EmitLoadReal(ILGenerator il, double value) =>
-            il.Emit(OpCodes.Ldc_R8, value);
     }
 }
