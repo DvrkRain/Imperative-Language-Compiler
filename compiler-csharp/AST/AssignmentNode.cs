08d3b54516c24a03062737282221b70007cf346e
Uberch <vam.molch@gmail.com>
2025-11-22 16:21:31 +0300

    Fix(codegen): Fix codegen on accessing fields of record

--- compiler-csharp/AST/AssignmentNode.cs
+++ compiler-csharp/AST/AssignmentNode.cs
@@ -58,110 +58,14 @@ public class AssignmentNode : Node {
 	}
     
 	
-    public override void Generate(CodeGen.CodeGenContext ctx)
-    {
-        var target = (FieldAccessNode)this.childs[0];
-        string baseName = (string)((PrimaryNode)target.GetChilds()[0]).value;
-        
-        // Simple variable assignment (no indexing/field access)
-        if (target.GetChilds().Count == 1)
-        {
-            // Generate right-hand side
-            this.childs[1].Generate(ctx);
-            
-            // Store to variable
-            if (ctx.LocalVariables.ContainsKey(baseName))
-                ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Stloc, ctx.LocalVariables[baseName]);
-            else if (ctx.GlobalFields.ContainsKey(baseName))
-                ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Stsfld, ctx.GlobalFields[baseName]);
-            else if (ctx.ParameterIndices.ContainsKey(baseName))
-            {
-                int argIndex = ctx.ParameterIndices[baseName];
-                if (argIndex <= 255)
-                    ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Starg_S, (byte)argIndex);
-                else
-                    ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Starg, argIndex);
-            }
-        }
-        else
-        {
-            // Complex assignment: arr[i] := value or record.field := value
-            GenerateComplexAssignment(ctx, target);
-        }
-    }
+    public override void Generate(CodeGen.CodeGenContext ctx) {
+		FieldAccessNode target = (FieldAccessNode)this.childs[0];
+		base.Generate(ctx);
 
-    private void GenerateComplexAssignment(CodeGen.CodeGenContext ctx, FieldAccessNode target)
-    {
-        string baseName = (string)((PrimaryNode)target.GetChilds()[0]).value;
-        
-        // Load base variable address
-        SystemType currentType = null;
-        if (ctx.LocalVariables.ContainsKey(baseName))
-        {
-            ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Ldloca, ctx.LocalVariables[baseName]);
-            currentType = ctx.LocalVariables[baseName].LocalType;
-        }
-        else if (ctx.GlobalFields.ContainsKey(baseName))
-        {
-            ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Ldsflda, ctx.GlobalFields[baseName]);
-            currentType = ctx.GlobalFields[baseName].FieldType;
-        }
-        
-        // Navigate to target (all but last)
-        for (int i = 1; i < target.GetChilds().Count - 1; i++)
-        {
-            var accessNode = target.GetChilds()[i];
-            
-            if (accessNode is ExpressionNode indexExpr)
-            {
-                // Array indexing
-                indexExpr.Generate(ctx);
-                currentType = currentType.GetElementType();
-            }
-            else if (accessNode is PrimaryNode fieldNode)
-            {
-                // Field access
-                string fieldName = (string)fieldNode.value;
-                var fieldInfo = currentType.GetField(fieldName);
-                currentType = fieldInfo.FieldType;
-            }
-        }
-        
-        // Last accessor determines store instruction
-        var lastAccess = target.GetChilds()[target.GetChilds().Count - 1];
-        
-        if (lastAccess is ExpressionNode finalIndex)
-        {
-            // Array element assignment: arr[i] := value
-            finalIndex.Generate(ctx);
-            this.childs[1].Generate(ctx); // Right-hand side value
-            
-            SystemType elementType = currentType.GetElementType();
-            EmitStoreElement(ctx.CurrentIL, elementType);
-        }
-        else if (lastAccess is PrimaryNode finalField)
-        {
-            // Record field assignment: record.field := value
-            string fieldName = (string)finalField.value;
-            var fieldInfo = currentType.GetField(fieldName);
-            
-            this.childs[1].Generate(ctx);
-            ctx.CurrentIL.Emit(System.Reflection.Emit.OpCodes.Stfld, fieldInfo);
-        }
-    }
+		if(target.GetChilds().Count() == 1)
+			ctx.CurrentIL.Emit(OpCodes.Stloc, target.variable.LocalIndex);
+		else if(target.GetChilds().Last() is PrimaryNode)
+			ctx.CurrentIL.Emit(OpCodes.Stfld, target.fieldInfo);
 
-    private void EmitStoreElement(System.Reflection.Emit.ILGenerator il, SystemType elementType)
-    {
-        if (elementType == typeof(int))
-            il.Emit(System.Reflection.Emit.OpCodes.Stelem_I4);
-        else if (elementType == typeof(double))
-            il.Emit(System.Reflection.Emit.OpCodes.Stelem_R8);
-        else if (elementType == typeof(bool))
-            il.Emit(System.Reflection.Emit.OpCodes.Stelem_I1);
-        else if (!elementType.IsValueType)
-            il.Emit(System.Reflection.Emit.OpCodes.Stelem_Ref);
-        else
-            il.Emit(System.Reflection.Emit.OpCodes.Stelem, elementType);
     }
-
 }
