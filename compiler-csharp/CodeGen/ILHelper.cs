a2951483d745e6c0c21bc3e0eb88b080ce9f4606
Uberch <vam.molch@gmail.com>
2025-11-23 13:09:59 +0300

    Fix(codegen): Fix value cast on different types

--- compiler-csharp/CodeGen/ILHelper.cs
+++ compiler-csharp/CodeGen/ILHelper.cs
@@ -23,11 +23,17 @@ namespace CodeGen
         public static void EmitLoadReal(ILGenerator il, bool value) =>
             il.Emit(OpCodes.Ldc_R8, value?1f:0f);
         
-        public static void EmitLoadBool(ILGenerator il, int value) =>
-            il.Emit(value != 0 ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
+        public static void EmitLoadBool(ILGenerator il, int value) {
+			if(value == 0)
+				il.Emit(OpCodes.Ldc_I4_0);
+			else if(value == 1)
+				il.Emit(OpCodes.Ldc_I4_1);
+			else
+				il.Emit(OpCodes.Throw);
+		}
         
-        public static void EmitLoadBool(ILGenerator il, float value) {
-			return; }
+        public static void EmitLoadBool(ILGenerator il, float value) =>
+			il.Emit(OpCodes.Throw);
         
         public static void EmitLoadBool(ILGenerator il, bool value) =>
             il.Emit(value ? OpCodes.Ldc_I4_1 : OpCodes.Ldc_I4_0);
